3546e35f3634e93f52005b7a23e1eb46739209cf
DomDarr <160680997+DomDarr@users.noreply.github.com>
2024-03-27 13:27:55 -0700

    Add files via upload
    Make sure you unzip nodemodules.zip

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const expressLayouts = require('express-ejs-layouts');
+const expressLayoutes = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
 const config = require('./startup/config');
 const winston = require('winston');
 const err = require('./middleware/errors');
 const customerRoutes = require('./routes/customer-routes');
-const recipeRoutes = require('./routes/recipeRoutes');
 const app = express();
 
 require('./startup/db')();
 require('./startup/logging')();
 require('./startup/validations')();
 
-app.use(expressLayouts);
-app.set('layout', './layouts/main');
+app.use(expressLayoutes);
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/recipes', recipeRoutes); // Using recipe routes
 app.use(customerRoutes.routes);
 app.use(err);
 
-const port = config.port || 8080; // Ensuring port is correctly set from config or default
-app.listen(port, () => winston.info(`App is listening on url http://localhost:${port}`)); // Changed to template string for dynamic port
+
+app.listen(config.port, () => winston.info('App is listening on url http://localhost:' + config.port));
--- models/customer.js
+++ models/customer.js
@@ -4,40 +4,32 @@ const Joi = require('joi');
 const customerSchema = new mongoose.Schema({
     firstname: {
         type: String,
-        minlength: 5,
+        minlength: 0,
         maxlength: 50,
         required: true
     },
-    lastname: {
-        type: String,
-        minlength: 5,
-        maxlength: 50,
-        required: true
-    }, 
+
     phonenumber: {
         type: String,
-        minlength: 10,
+        minlength: 0,
         required: true
     },
     cnic: {
         type: String,
         required: true
     }, 
-    address: {
-        type: String,
-        required: true
-    }
+ 
 });
 
 const Customer = mongoose.model('Customer', customerSchema);
 
 const validateCustomer = (customer) => {
     const schema = Joi.object({
-        firstname: Joi.string().min(5).max(50).required(),
-        lastname: Joi.string().min(5).max(50).required(),
-        phonenumber: Joi.string().min(10).required(),
+        firstname: Joi.string().required(),
+  
+        phonenumber: Joi.required(),
         cnic: Joi.string().required(),
-        address: Joi.string().required()
+
     })
 
     return schema.validate(customer)
